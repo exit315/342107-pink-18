b8060c547d5638db8fd298238194a7dfc41a43fc
Anastasiia <karn-av@protonmail.com>
2019-11-30 22:21:10 +0300

    Правка разметки

--- source/js/util.js
+++ source/js/util.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var pageHeader = document.querySelector('.page-header')
+var pageHeader = document.querySelector('.page-header');
+var pageHeaderModifier = pageHeader.querySelector('.page-header__header-logo-wrapper');
 var mainNav = document.querySelector('.main-nav');
 var siteList = mainNav.querySelector('.site-list');
 var mobileNavToggle = pageHeader.querySelector('.main-nav__toggle');
@@ -8,6 +9,7 @@ var mobileNavToggle = pageHeader.querySelector('.main-nav__toggle');
 var onMobileNavToggleClick = function (evt) {
   evt.preventDefault();
   siteList.classList.toggle('hidden');
+  pageHeaderModifier.classList.toggle('mobile-nav-opened');
 }
 
 mobileNavToggle.addEventListener('click', onMobileNavToggleClick);
